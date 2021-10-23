8377a04e2943c167645e56e9bfe7571abd5d174e
Valdis Prodnieks <valdis.prodnieks@emergn.com>
2021-10-23 23:36:52 +0300

    basket put in single page application manner

---
+++ bambinomio/assets/js/basket.js
@@ -0,0 +1,78 @@
+function addToBasket(id, name, price, vat, img) {
+    // search if the product is already in basket
+    var product = basketData.filter(obj => {
+        return obj.id === id
+    })
+    if (product.length > 0) {  // we just increase the count
+        product[0].count += 1;
+    } else {  // the product is not yet in basket
+        basketData.push({
+            "id": id,
+            "name": name,
+            "price": price,
+            "vat": vat,
+            "count": 1,
+            "img": img,
+        });
+    }
+    console.log("---- added to basket:", basketData);
+}
+
+function showBasket(data) {
+    var basketTable = document.getElementById("basketTable");
+    var basketTableForShow = document.createElement("table");
+
+    // EXTRACT VALUE FOR HTML HEADER. 
+    // ('id', 'name', 'price', "vat", "count", "img")
+    var col = [];
+    for (var i = 0; i < data.length; i++) {
+        for (var key in data[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
+            }
+        }
+    }
+
+    // insert header
+    let tr = basketTableForShow.insertRow();         // insert row at table end
+    let th = document.createElement("th");
+    th.innerHTML = "Preces nosaukums";
+    tr.appendChild(th);
+    th = document.createElement("th");
+    th.innerHTML = "Cena";
+    tr.appendChild(th);
+    th = document.createElement("th");
+    th.innerHTML = "PVN";
+    tr.appendChild(th);
+    th = document.createElement("th");
+    th.innerHTML = "skaits";
+    tr.appendChild(th);
+    th = document.createElement("th");
+    th.innerHTML = "Attēls";
+    tr.appendChild(th);
+
+    basketTableForShow.innerHTML = "";
+    basketTableForShow.appendChild(tr);
+
+    // append data
+    for (let i = 0; i < data.length; i++) {
+        tr = basketTableForShow.insertRow(); // insert row at table end
+
+        for (var j = 1; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            if (j === col.length - 1) {  // last row contains picture
+                productPicture = document.createElement("img");
+                productPicture.src = data[i][col[j]];
+                productPicture.style.width = "30%";
+                tabCell.appendChild(productPicture);
+            } else {
+                tabCell.innerHTML = data[i][col[j]];
+            }
+        }
+    }
+
+    basketTable.innerHTML = "";
+    basketTable.appendChild(basketTableForShow);
+    console.log("---- 010 basketTableForShow:", basketTableForShow);
+
+}
