eab773233f23750002dce0902fdcf7ab95d2e832
Valdis Prodnieks <valdis.prodnieks@emergn.com>
2021-11-16 21:43:32 +0200

    omniva added

--- bambinomio/assets/js/navigation.js
+++ bambinomio/assets/js/navigation.js
@@ -1,8 +1,26 @@
 function toggleNavBar() {
-    var x = document.getElementById("navig-small-screens");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else {
-      x.className = x.className.replace(" w3-show", "");
-    }
+  toggleElement("navig-small-screens");
+}
+
+function toggleElement(id) {
+  const x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
   }
+}
+
+function showElement(id) {
+  const x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } 
+}
+
+function hideElement(id) {
+  const x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") != -1) {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
