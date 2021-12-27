3599067ddb7da511ff925f9a35de0c17339abc1f
Valdis Prodnieks <valdis.prodnieks@emergn.com>
2021-12-28 01:23:29 +0200

    piegade

--- bambinomio/assets/js/navigation.js
+++ bambinomio/assets/js/navigation.js
@@ -15,7 +15,8 @@ function showElement(id) {
   const x = document.getElementById(id);
   if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
-  } 
+  }
+  x.disabled = false; 
 }
 
 function hideElement(id) {
@@ -23,4 +24,5 @@ function hideElement(id) {
   if (x.className.indexOf("w3-show") != -1) {
     x.className = x.className.replace(" w3-show", "");
   }
+  x.disabled = true; 
 }
