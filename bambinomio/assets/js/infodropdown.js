f9ac4806767ff1680281309365c5640823961a4f
egrabovskis <ernests243@gmail.com>
2022-02-06 20:17:06 +0200

    ernests-hidden-info-and-added-pictures

---
+++ bambinomio/assets/js/infodropdown.js
@@ -0,0 +1,44 @@
+
+function ShowAndHidePrivacy() {
+    var x = document.getElementById('privacyinfo');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function ShowAndHideDiliveryInfo() {
+    var x = document.getElementById('diliveryinfo');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function ShowAndHideAboutDiapers() {
+    var x = document.getElementById('aboutdiapersinfo');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function ShowAndHideTutorialInfo() {
+    var x = document.getElementById('tutorialinfo');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function ShowAndHideContactsInfo() {
+    var x = document.getElementById('contactsinfo');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
