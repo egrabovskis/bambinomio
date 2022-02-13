03b275a07e1d179a8b0faccbd2ccc2bf097aff8d
egrabovskis <ernests243@gmail.com>
2022-02-13 14:40:20 +0200

    ernests/added-prices-and-sizes

--- bambinomio/assets/js/data/pottytraining.js
+++ bambinomio/assets/js/data/pottytraining.js
@@ -5,135 +5,136 @@ var pottyTrainingData =
     "price": 15.20,
     "colors": [
         {
-            "name": "Zebra cadabra",
-            "img": "assets/img/pottytraining/potty-training-pants-zebra-cadabra-web_900x.jpg",
-            "sizes": [ "large"]
+            "name": "captain-carrot",
+            "img": "assets/img/pottytraining/potty-training-pants-captain-carrot-web_900x.jpg",
+            "sizes": ["little", "large"]
         },
         {
-            "name": "White",
-            "img": "assets/img/pottytraining/potty-training-pants-white-web_1_900x.jpg",
-            "sizes": ["large", "medium"]
-        }, 
+            "name": "Zebra cadabra",
+            "img": "assets/img/pottytraining/potty-training-pants-zebra-cadabra-web_900x.jpg",
+            "sizes": ["little", "medium", "large"]
+        },
+        // {
+        //     "name": "White",
+        //     "img": "assets/img/pottytraining/potty-training-pants-white-web_1_900x.jpg",
+        //     "sizes": ["large", "medium"]
+        // }, 
         {
             "name": "zemenes",
             "img": "assets/img/pottytraining/potty-training-pants-web_900x.jpg",
-            "sizes": ["medium", "little"]
+            "sizes": ["little", "medium", "large"]
         },
-        {
-            "name": "tropical-island",
-            "img": "assets/img/pottytraining/potty-training-pants-tropical-island-web_900x.jpg",
-            "sizes": ["medium", "small"]
-        },
+        // {
+        //     "name": "tropical-island",
+        //     "img": "assets/img/pottytraining/potty-training-pants-tropical-island-web_900x.jpg",
+        //     "sizes": ["medium", "small"]
+        // },
         {
             "name": "totally-roarsome",
             "img": "assets/img/pottytraining/potty-training-pants-totally-roarsome-web_900x.jpg",
-            "sizes": ["medium", "small"]
+            "sizes": ["medium", "large"]
         },
-        {
-            "name": "teal",
-            "img": "assets/img/pottytraining/potty-training-pants-teal-web_3_900x.jpg",
-            "sizes": ["medium", "small"]
-        },
-        {
-            "name": "ruby",
-            "img": "assets/img/pottytraining/potty-training-pants-ruby-web_900x.jpg",
-            "sizes": ["medium", "small"]
-        },
+        // {
+        //     "name": "teal",
+        //     "img": "assets/img/pottytraining/potty-training-pants-teal-web_3_900x.jpg",
+        //     "sizes": ["medium", "small"]
+        // },
+        // {
+        //     "name": "ruby",
+        //     "img": "assets/img/pottytraining/potty-training-pants-ruby-web_900x.jpg",
+        //     "sizes": ["medium", "small"]
+        // },
         {
             "name": "puddle-pig",
             "img": "assets/img/pottytraining/potty-training-pants-puddle-pigs-web_900x.jpg",
-            "sizes": ["medium", "small"]
+            "sizes": ["little", "medium",]
         },
-        {
-            "name": "pink-elephant",
-            "img": "assets/img/pottytraining/potty-training-pants-pink-elephant-web_2_900x.jpg",
-            "sizes": ["medium", "small"]
-        },
+        // {
+        //     "name": "pink-elephant",
+        //     "img": "assets/img/pottytraining/potty-training-pants-pink-elephant-web_2_900x.jpg",
+        //     "sizes": ["medium", "small"]
+        // },
         {
             "name": "pegasus-palace-house",
             "img": "assets/img/pottytraining/potty-training-pants-pegasus-palace-house-web_2_900x.jpg",
-            "sizes": ["medium", "small"]
+            "sizes": ["little", "medium"]
         },
         {
             "name": "pea-power",
             "img": "assets/img/pottytraining/potty-training-pants-pea-power-web_900x.jpg",
-            "sizes": ["medium", "small"]
+            "sizes": ["little", "medium", "large"]
         },
         {
             "name": "outer-space",
             "img": "assets/img/pottytraining/potty-training-pants-outer-space-web_2_900x.jpg",
-            "sizes": []
+            "sizes": ["little", "medium"]
         },
         {
             "name": "moo-meadow",
             "img": "assets/img/pottytraining/potty-training-pants-moo-meadow-web_900x.jpg",
-            "sizes": []
+            "sizes": ["little", "medium"]
         },
         {
             "name": "lilac",
             "img": "assets/img/pottytraining/potty-training-pants-lilac-web_5_900x.jpg",
-            "sizes": []
+            "sizes": ["medium"]
         },
         {
             "name": "light-pink",
             "img": "assets/img/pottytraining/potty-training-pants-light-pink-web_900x.jpg",
-            "sizes": []
+            "sizes": ["medium", "large"]
         },
         {
             "name": "hen-house",
             "img": "assets/img/pottytraining/potty-training-pants-hen-house-web_900x.jpg",
-            "sizes": []
+            "sizes": ["little", "medium"]
         },
         {
             "name": "giraffing-around",
             "img": "assets/img/pottytraining/potty-training-pants-giraffing-around-web_900x.jpg",
-            "sizes": []
-        }, 
+            "sizes": ["little", "medium", "large"]
+        },
         {
             "name": "farmer-friends",
             "img": "assets/img/pottytraining/potty-training-pants-farmer-friends-web_900x.jpg",
-            "sizes": []
-        }, 
+            "sizes": ["little", "medium"]
+        },
         {
             "name": "fairy",
             "img": "assets/img/pottytraining/potty-training-pants-fairy-web_2_900x.jpg",
-            "sizes": []
-        }, 
+            "sizes": ["little", "medium"]
+        },
         {
             "name": "emerald",
             "img": "assets/img/pottytraining/potty-training-pants-emerald-web_900x.jpg",
-            "sizes": []
-        }, 
+            "sizes": ["little", "medium", "large"]
+        },
         {
             "name": "elphantastic",
             "img": "assets/img/pottytraining/potty-training-pants-elphantastic-web_720x.jpg",
-            "sizes": []
-        }, 
-        {
-            "name": "dragons-dungeon",
-            "img": "assets/img/pottytraining/potty-training-pants-dragons-dungeon-web_6_900x.jpg",
-            "sizes": []
-        }, 
+            "sizes": ["medium", "large"]
+        },
+        // {
+        //     "name": "dragons-dungeon",
+        //     "img": "assets/img/pottytraining/potty-training-pants-dragons-dungeon-web_6_900x.jpg",
+        //     "sizes": []
+        // }, 
         {
             "name": "pants-dino",
             "img": "assets/img/pottytraining/potty-training-pants-dino-web_2_900x.jpg",
-            "sizes": []
-        }, 
-        {
-            "name": "captain-carrot",
-            "img": "assets/img/pottytraining/potty-training-pants-captain-carrot-web_900x.jpg",
-            "sizes": []
-        }, 
+            "sizes": ["medium", "large"]
+        },
+
         {
             "name": "bolt-bonanza",
             "img": "assets/img/pottytraining/potty-training-pants-bolt-bonanza-web_900x.jpg",
-            "sizes": []
-        }, 
-        {
-            "name": "blue",
-            "img": "assets/img/pottytraining/potty-training-pants-blue-web_2_900x.jpg",
-            "sizes": []
-        }, 
+            "sizes": ["little"]
+        },
+        // {
+        //     "name": "blue",
+        //     "img": "assets/img/pottytraining/potty-training-pants-blue-web_2_900x.jpg",
+        //     "sizes": []
+        // }, 
         {
             "name": "",
             "img": "assets/img/pottytraining/diagram-of-potty-training-pants-usa_d80ca5fa-b707-49c6-9d8d-dd737ebf2e48_900x.jpg",
