88961aa7b0bc06aa33b08e109145d198f32b5aca
egrabovskis <ernests243@gmail.com>
2022-02-24 18:48:35 +0200

    bug fixes

--- bambinomio/assets/js/infodropdown.js
+++ bambinomio/assets/js/infodropdown.js
@@ -9,9 +9,9 @@ function ShowAndHidePrivacy() {
 }
 
 function ShowAndHideDeliveryInfo() {
-    var x = document.getElementById('deliveryinfo');
+    var x = document.getElementById('deliveryinfo0');
     if (x.style.display == 'none') {
-        x.style.display = 'block';
+        x.style.display = 'block'; 
     } else {
         x.style.display = 'none';
     }
